3080fee699490f86173ce3f8c892139f1b3ac952
Biinod81 <thomas.ly@iologo.io>
2022-07-04 09:20:47 +0200

    modernized front

--- frontend/src/components/ScheduleCard/ScheduleCard.jsx
+++ frontend/src/components/ScheduleCard/ScheduleCard.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 
 import './ScheduleCard.css'
 
-const ScheduleCard = ({ title, code, teacher }) => {
+const ScheduleCard = ({ title, code, teacher, date }) => {
   return (
-    <div className='scheduleCard__body'>
-      <h4>{code}</h4>
-      <h5>{title}</h5>
-      <span>{teacher}</span>
+    <div style={{padding: '0.5rem'}}>
+      {date ? <div className='scheduleCard__date'>{date}</div> : null}
+      <div className='scheduleCard__body'>
+        <h4 style={{color: '#c1002a'}}>{code}</h4>
+        <h5>{title}</h5>
+        <span>{teacher}</span>
+      </div>
     </div>
   )
 }
--- frontend/src/pages/schedule/schedule.jsx
+++ frontend/src/pages/schedule/schedule.jsx
@@ -7,38 +7,43 @@ const schedule = () => {
   return (
     <div className='container mt-3'>
       <div className='schedule__top'>
-        <div className='schedule__time'>
+        <div className='schedule__time' style={{marginTop: '65px', marginBottom: '5px'}}>
           <span>9h00</span>
           <span>12h30</span>
         </div>
-        <ScheduleCard 
+        <ScheduleCard
           title="Eval de perf. & sûreté de fonct."
           code="RCP103"
-          teacher="JF.Culus"
+          teacher="JF.CULUS"
+          date="Lundi 4 Juillet"
         />
         <ScheduleCard 
-          title="Eval de perf. & sûreté de fonct."
-          code="RCP103"
-          teacher="JF.Culus"
+          title="Management d'entreprise et RSE"
+          code="MSE147"
+          teacher="A.GRAMONT"
+          date="Mardi 5 Juillet"
         />
         <ScheduleCard 
-          title="Eval de perf. & sûreté de fonct."
-          code="RCP103"
-          teacher="JF.Culus"
+          title="Architecture des systèmes info."
+          code="NSY104"
+          teacher="S.BARAKETI"
+          date="Mercredi 6 Juillet"
         />
         <ScheduleCard 
-          title="Eval de perf. & sûreté de fonct."
-          code="RCP103"
-          teacher="JF.Culus"
+          title="Projet"
+          code="PRJ102"
+          teacher="L.CHAARI"
+          date="Jeudi 7 Juillet"
         />
         <ScheduleCard 
-          title="Eval de perf. & sûreté de fonct."
-          code="RCP103"
-          teacher="JF.Culus"
+          title="Architecture des systèmes info."
+          code="NSY104"
+          teacher="S.BARAKETI"
+          date="Vendredi 8 Juillet"
         />
       </div>
       <div className='schedule__bot'>
-        <div className='schedule__time'>
+        <div className='schedule__time' style={{marginTop: '10px', marginBottom: '5px'}}>
           <span>13h30</span>
           <span>17h00</span>
         </div>
